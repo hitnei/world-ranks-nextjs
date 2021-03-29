54af8827a01379bdbbba10a1d0b6d50d2c535385
nhi-hp <77150808+nhi-hp@users.noreply.github.com>
2021-03-29 16:57:19 +0700

    layout + Search input + get static props coutries

---
+++ world-ranks/src/pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import CountriesTable from '../components/CountriesTable/CountriesTable';
+import SearchInput from '../components/SearchInput/SearchInput';
+import styles from '../styles/Home.module.css'
+import Layout from './../components/Layout/Layout';
+
+export default function Home({ countries }) {
+  return (
+    <Layout>
+      <div className={styles.counts}>Found {countries.length} countries</div>
+      <SearchInput placeholder="Filter by Name, Region or SubRegion" />
+      <CountriesTable countries={countries}/>
+    </Layout>
+  )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://restcountries.eu/rest/v2/all')
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    }
+  }
+}
